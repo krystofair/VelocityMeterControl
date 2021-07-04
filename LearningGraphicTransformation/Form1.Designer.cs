4c86f03dcb3599a8aa8b50e25c226b497868a1e9
Krystofer Lando <krystofer02@gmail.com>
2021-07-04 02:29:44 +0200

    base ver 0.5

---
+++ LearningGraphicTransformation/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace LearningGraphicTransformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vMeter1 = new LearningGraphicTransformation.VMeter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vMeter1
+            // 
+            this.vMeter1.BackColor = System.Drawing.Color.Black;
+            this.vMeter1.LoadExtra = false;
+            this.vMeter1.Location = new System.Drawing.Point(27, 38);
+            this.vMeter1.LocationDir = "D:\\LearningGraphicTransformation\\LearningGraphicTransformation\\vmet-mat";
+            this.vMeter1.Name = "vMeter1";
+            this.vMeter1.Size = new System.Drawing.Size(613, 613);
+            this.vMeter1.TabIndex = 0;
+            this.vMeter1.Text = "vMeter1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(680, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "w góre";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(680, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "w dół";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 661);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vMeter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VMeter vMeter1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LearningGraphicTransformation/VMeter.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LearningGraphicTransformation
+{
+    public partial class VMeter : Control
+    {
+        private string _location_files_dir_path = "";
+        private static readonly string[] _img_names = { "base.png", "middle.png", "pointer.png" };
+        private List<Bitmap> _img_files = new List<Bitmap>();
+        public event PropertyChangedEventHandler LocationDirChange;
+        public event PropertyChangedEventHandler ExtraChange;
+        private bool _load_extra=false;
+        private float where=0;
+
+        public bool LoadExtra
+        {
+            set { _load_extra = value; ExtraChange.Invoke(this, new PropertyChangedEventArgs("LoadExtra")); }
+            get { return _load_extra; }
+        }
+        public Bitmap GetBase()
+        {
+            if (_img_files.Count >= 1) return _img_files[0];
+            else return null;
+        }
+        public Bitmap GetPointer()
+        {
+            if (_img_files.Count >= 2) return _img_files[^1];
+            else return null;
+        }
+        public Bitmap GetExtra()
+        {
+            if (_img_files.Count == 3) return _img_files[1];
+            else return null;
+        }
+
+        public string LocationDir
+        {
+            set {
+                if (value != "")
+                {
+                    _location_files_dir_path = value;
+                    LocationDirChange.Invoke(this, new PropertyChangedEventArgs("LocationDir"));
+                }
+            }
+            get { return _location_files_dir_path; }
+        }
+        protected override Size DefaultSize
+        {
+            get { return new Size(200, 200); }
+        }
+        public VMeter()
+        {
+            InitializeComponent();
+            BackColor = Color.Black;
+            ExtraChange = new PropertyChangedEventHandler(OnExtraChanged);
+            LocationDirChange = new PropertyChangedEventHandler(OnLocationDirChanged);
+        }
+        private void OnExtraChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnLocationDirChanged(sender, e);
+        }
+
+        private void OnLocationDirChanged(object sender, PropertyChangedEventArgs e)
+        {
+            _img_files.Clear();
+            try
+            {
+                string path = _location_files_dir_path;
+                if (!(_location_files_dir_path.EndsWith('\\')
+                    || _location_files_dir_path.EndsWith('/')))
+                    path += "\\";
+                Bitmap base_bp = new Bitmap(Image.FromFile(path + _img_names[0]));
+                float sx = (this.Size.Width / (float)base_bp.Width) * base_bp.Width;
+                float sy = (this.Size.Width / (float)base_bp.Height) * base_bp.Height;
+                _img_files.Add(new Bitmap(base_bp, new Size((int)sx, (int)sy)));
+                if (_load_extra)
+                {
+                    Bitmap extra_bp = new Bitmap(Image.FromFile(path + _img_names[1]));
+                    _img_files.Add(new Bitmap(extra_bp, new Size((int)sx, (int)sy)));
+                }
+                Bitmap pointer_bp = new Bitmap(Image.FromFile(path + _img_names[2]));
+                _img_files.Add(new Bitmap(pointer_bp, new Size((int)sx, (int)sy)));
+            }
+            catch
+            {
+                Console.WriteLine("Error with found images.");
+            }
+        }
+        public void ChangePos(int v)
+        {
+            where = v;
+            Invalidate();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            this.Size = new Size(this.Size.Width, this.Size.Width); // potem poprawić.
+            OnLocationDirChanged(this, new PropertyChangedEventArgs("LocationDir"));
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            if(_location_files_dir_path != "")
+            {
+                try
+                {
+                    if (_img_files.Count >= 1)
+                    {
+                        pe.Graphics.DrawImage(_img_files[0], 0, 0);//Size.Width/2, Size.Height/2);
+                        if(_load_extra) pe.Graphics.DrawImage(_img_files[1], 0, 0);
+                        pe.Graphics.TranslateTransform(this.ClientRectangle.Width/2, this.ClientRectangle.Height/2);
+                        pe.Graphics.RotateTransform(where);
+                        pe.Graphics.DrawImage(_img_files[^1], -1*this.ClientRectangle.Width / 2, -1*this.ClientRectangle.Height / 2);
+                    }
+                }
+                catch
+                {   
+                }
+            }
+        }
+    }
+}
