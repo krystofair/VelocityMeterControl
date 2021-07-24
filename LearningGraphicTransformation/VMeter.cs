c880e62b617afc901a176d8e838d65a02c29e978
Krystofer Lando <krystofer02@gmail.com>
2021-07-24 13:10:37 +0200

    Change way of loading files, now those are loaded from resource

--- LearningGraphicTransformation/VMeter.cs
+++ LearningGraphicTransformation/VMeter.cs
@@ -10,10 +10,7 @@ namespace LearningGraphicTransformation
 {
     public partial class VMeter : Control
     {
-        private string _location_files_dir_path = "";
-        private static readonly string[] _img_names = { "base.png", "middle.png", "pointer.png" };
         private List<Bitmap> _img_files = new List<Bitmap>();
-        public event PropertyChangedEventHandler LocationDirChange;
         public event PropertyChangedEventHandler ExtraChange;
         private bool _load_extra=false;
         private float where=0;
@@ -38,18 +35,6 @@ namespace LearningGraphicTransformation
             if (_img_files.Count == 3) return _img_files[1];
             else return null;
         }
-
-        public string LocationDir
-        {
-            set {
-                if (value != "")
-                {
-                    _location_files_dir_path = value;
-                    LocationDirChange.Invoke(this, new PropertyChangedEventArgs("LocationDir"));
-                }
-            }
-            get { return _location_files_dir_path; }
-        }
         protected override Size DefaultSize
         {
             get { return new Size(200, 200); }
@@ -59,7 +44,6 @@ namespace LearningGraphicTransformation
             InitializeComponent();
             BackColor = Color.Black;
             ExtraChange = new PropertyChangedEventHandler(OnExtraChanged);
-            LocationDirChange = new PropertyChangedEventHandler(OnLocationDirChanged);
         }
         private void OnExtraChanged(object sender, PropertyChangedEventArgs e)
         {
@@ -71,23 +55,19 @@ namespace LearningGraphicTransformation
             _img_files.Clear();
             try
             {
-                string path = _location_files_dir_path;
-                if (!(_location_files_dir_path.EndsWith('\\')
-                    || _location_files_dir_path.EndsWith('/')))
-                    path += "\\";
-                Bitmap base_bp = new Bitmap(Image.FromFile(path + _img_names[0]));
-                float sx = (this.Size.Width / (float)base_bp.Width) * base_bp.Width;
-                float sy = (this.Size.Width / (float)base_bp.Height) * base_bp.Height;
+                Bitmap base_bp = new Bitmap(Properties.Resources._base);
+                int sx = (int)((this.Size.Width / (float)base_bp.Width) * base_bp.Width);
+                int sy = (int)((this.Size.Width / (float)base_bp.Height) * base_bp.Height);
                 _img_files.Add(new Bitmap(base_bp, new Size((int)sx, (int)sy)));
                 base_bp.Dispose();
                 if (_load_extra)
                 {
                     _img_files.Add(
-                        new Bitmap(Image.FromFile(path + _img_names[1]), new Size((int)sx, (int)sy))
+                        new Bitmap(Properties.Resources.middle, new Size(sx, sy))
                         );
                 }
                 _img_files.Add(
-                        new Bitmap(Image.FromFile(path + _img_names[2]), new Size((int)sx, (int)sy))
+                        new Bitmap(Properties.Resources.pointer, new Size(sx, sy))
                         );
             }
             catch
@@ -112,22 +92,19 @@ namespace LearningGraphicTransformation
         protected override void OnPaint(PaintEventArgs pe)
         {
             base.OnPaint(pe);
-            if(_location_files_dir_path != "")
+            try
             {
-                try
+                if (_img_files.Count >= 1)
                 {
-                    if (_img_files.Count >= 1)
-                    {
-                        pe.Graphics.DrawImage(_img_files[0], 0, 0);//Size.Width/2, Size.Height/2);
-                        if(_load_extra) pe.Graphics.DrawImage(_img_files[1], 0, 0);
-                        pe.Graphics.TranslateTransform(this.ClientRectangle.Width/2, this.ClientRectangle.Height/2);
-                        pe.Graphics.RotateTransform(where);
-                        pe.Graphics.DrawImage(_img_files[^1], -1*this.ClientRectangle.Width / 2, -1*this.ClientRectangle.Height / 2);
-                    }
+                    pe.Graphics.DrawImage(_img_files[0], 0, 0);//Size.Width/2, Size.Height/2);
+                    if (_load_extra) pe.Graphics.DrawImage(_img_files[1], 0, 0);
+                    pe.Graphics.TranslateTransform(this.ClientRectangle.Width / 2, this.ClientRectangle.Height / 2);
+                    pe.Graphics.RotateTransform(where);
+                    pe.Graphics.DrawImage(_img_files[^1], -1 * this.ClientRectangle.Width / 2, -1 * this.ClientRectangle.Height / 2);
                 }
-                catch
-                {   
-                }
+            }
+            catch
+            {
             }
         }
     }
